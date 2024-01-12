a61dff3fde027e86d892efdc8230ac6d5a969581
SeannaCHARLES <152272068+SeannaCHARLES@users.noreply.github.com>
2024-01-12 11:29:00 +0100

    Update distrib_theta.R

--- src/distrib_theta.R
+++ src/distrib_theta.R
@@ -1,26 +1,29 @@
 library(rjson)
 #library(pdftools)
-input_path <- 
+
 output_filename <- commandArgs(trailingOnly = TRUE)[2]
 
 data_DSSR <- fromJSON(file=commandArgs(trailingOnly = TRUE)[1])
+resultats_file <-commandArgs(trailingOnly = TRUE)[3]
 
 
-
-distribution<-function(data_json){
-  theta<-c()
-  for(rp in names(data_json)) {
-  theta<-c(theta,na.omit(as.numeric(data_json[[rp]]$angles$theta)))
+distribution <- function(data_json) {
+  theta <- c()
+  names_list <- character(length = length(data_json))  # Initialize a character vector to store names
+  for (i in seq_along(data_json)) {
+    rp <- names(data_json)[i]
+    theta <- c(theta, na.omit(as.numeric(data_json[[rp]]$angles$theta)))
+    names_list[i] <- rp
   }
-  return(theta)
+  return(list(theta = theta, names_list = names_list))
 }
 
-classes <- function(theta_data) {
+classes <- function(theta_data, rp) {
   val_min <- min(theta_data)
   val_max <- max(theta_data)
-  path<-paste0("/home/sea/Desktop/Fariza/Angle_RNA/ENV_angle_RNA/rna_angles_prediction_dssr/output_dir",output_filename)
-  pdf(output_filename) 
-
+  path <- paste0("data/output", output_filename)
+  pdf(file = path)  # Specify the file path for the PDF
+  
   nb_classes <- 1 + log2(length(theta_data))
   classes <- cut(theta_data, breaks = nb_classes)
   min_max_values <- tapply(theta_data, classes, function(x) c(min(x), max(x)))
@@ -31,11 +34,22 @@ classes <- function(theta_data) {
          col = c("red", "red"), bty = "n", cex = 0.8)
   legend("top", legend = paste("Classes:", levels(classes)), fill = "lightblue", bty = "n", cex = 0.8)
   dev.off()
+  
+
+  cat(resultats_file, "Classes:", levels(classes), "\n\n",file=resultats_file)
 }
 
-theta_train<-distribution(data_DSSR)
+# Extracting values and names from the distribution function
+result <- distribution(data_DSSR)
+theta_train <- result$theta
+names_list <- result$names_list
 
+# Iterating through names to call the classes function
+for (i in seq_along(names_list)) {
+  train <- classes(theta_train, names_list[i])
+}
 
-train<-classes(theta_train)
+# Close the resultats_file
+close(resultats_file)
 
 
