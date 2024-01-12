26410d7c1c30603493160997f29f89eaa41ecaae
SeannaCHARLES <152272068+SeannaCHARLES@users.noreply.github.com>
2024-01-12 12:31:11 +0100

    Update multifasta_matrix.py

--- src/multifasta_matrix.py
+++ src/multifasta_matrix.py
@@ -7,10 +7,9 @@ def read_fasta(file_path):
     Read a FASTA file and return the header and sequence as strings.
     """
     with open(file_path, 'r') as file:
-        # Skip the first line (header)
         header = file.readline().strip()
 
-        # Read the rest of the file and join lines into a single string
+
         sequence = ''.join(line.strip() for line in file.readlines())
 
     return header, sequence
@@ -41,23 +40,20 @@ def process_fasta_files(fasta_files):
     sequences = []
     headers = []
 
-    # Read sequences and headers from FASTA files
     for fasta_file in fasta_files:
         header, sequence = read_fasta(fasta_file)
         normalized_sequence = normalize_fasta_sequence(sequence)
         sequences.append(normalized_sequence)
         headers.append(header)
 
-    # Find the maximum length among all sequences
+
     max_sequence_length = max(len(sequence) for sequence in sequences)
 
-    # Initialize the list of one-hot matrices
+
     one_hot_matrices = []
 
-    # Perform one-hot encoding and fill the matrices
     for sequence in sequences:
         one_hot_matrix = one_hot_encoding(sequence)
-        # Add zeros to the end to align the matrices
         padding_length = max_sequence_length - len(sequence)
         padding_matrix = np.zeros((padding_length, 4), dtype=int)
         one_hot_matrix = np.vstack([one_hot_matrix, padding_matrix])
@@ -66,22 +62,21 @@ def process_fasta_files(fasta_files):
     return headers, one_hot_matrices
 
 def main():
-    # Specify the path to the folder containing input FASTA files
-    fasta_folder_path = 'TestSet_seq_Fasta'
 
-    # List of FASTA files in the folder
+    fasta_folder_path = 'data/TestSet_seq_Fasta'
+
+
     fasta_files = [os.path.join(fasta_folder_path, file) for file in os.listdir(fasta_folder_path) if file.endswith('.fasta')]
 
     try:
-        # Process FASTA files
         headers, one_hot_matrices = process_fasta_files(fasta_files)
 
-        # Create a folder to save CSV files
-        output_folder = 'Test_matrices'
+ 
+        output_folder = 'data/Test_matrices'
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
 
-        # Save each matrix in a separate CSV file with the corresponding header
+     
         for i, (header, matrix) in enumerate(zip(headers, one_hot_matrices)):
             columns = [f'nucleotide_{j}' for j in range(1, matrix.shape[1] + 1)]
             df = pd.DataFrame(matrix, columns=columns)
